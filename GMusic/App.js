32cf2f4a9c650682ab18b628775fea91f7d01c28
GalloJunior <gallojunior@gmail.com>
2025-08-28 11:47:53 -0300

    Criando o player

---
+++ GMusic/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const { width, height } = Dimensions.get('window');
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.main}>
+
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.iconWrapper}>
+          <TouchableOpacity>
+            <Ionicons name='heart-outline' size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='repeat' size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='share-outline' size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='ellipsis-horizontal' size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222831',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
