ba80be3e2c5618e7c9072b0950ade57a93d2bdbf
GalloJunior <gallojunior@gmail.com>
2025-09-08 11:42:04 -0300

    Adicionando Flatlist

--- GMusic/App.js
+++ GMusic/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image, FlatList } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Slider from '@react-native-community/slider';
+import songs from './model/data';
 
 const { width, height } = Dimensions.get('window');
 
 export default function App() {
+  const renderSongs = ({ item, index }) => {
+    return (
+      <View style={styles.mainImageWrapper}>
+        <View style={[styles.imageWrapper, styles.elevation]}>
+          <Image source={item.artwork} style={styles.musicImage}/>
+        </View>
+      </View>
+    )
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
 
-        <View style={[styles.imageWrapper, styles.elevation]}>
-          <Image
-            source={require('./assets/img/gallo.png')}
-            style={styles.musicImage}
-          />
-        </View>
+        <FlatList
+          data={songs}
+          keyExtractor={item => item.id}
+          renderItem={renderSongs}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+          onScroll={() => { }}
+        />
 
         <View>
           <Text style={[styles.songContent, styles.songTitle]}>
@@ -36,7 +51,7 @@ export default function App() {
             thumbTintColor='#FFD369'
             minimumTrackTintColor='#FFD369'
             maximumTrackTintColor='#fff'
-            onSlidingComplete={() => {}}
+            onSlidingComplete={() => { }}
           />
           <View style={styles.progressLevelDuration}>
             <Text style={styles.progressLabelText}>00:00</Text>
@@ -56,7 +71,7 @@ export default function App() {
           </TouchableOpacity>
         </View>
 
-      </View> 
+      </View>
       <View style={styles.footer}>
         <View style={styles.iconWrapper}>
           <TouchableOpacity>
@@ -87,6 +102,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mainImageWrapper : {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   footer: {
     width: width,
